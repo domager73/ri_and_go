3e4b290cb9000b3c3f3d6bb3b5d690c2901396b8
Jason1258t <ivandomogatskij@gmail.com>
2023-04-09 13:31:40 +0300

    Create authTextStyles.dart

---
+++ lib/Theme/authTextStyles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class LoginTextes {
+  static const buttonTextStyleActive = TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Roboto',
+      decoration: TextDecoration.underline,
+      decorationThickness: 2,
+      decorationColor: Color(0xff854000),
+      color: Color(0xffB97003)
+  );
+  static const buttonTextStyle = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+    color: Color(0xffA6A6A6),
+  );
+}
